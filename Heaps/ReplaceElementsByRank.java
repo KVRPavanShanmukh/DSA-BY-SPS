b0d09e5390aec0c3b3876962d9b5fbff2069f54d
KVRPavanShanmukh <2300090201@kluniversity.in>
2026-07-26 20:51:14 +0530

    Learnt Priority Queues and completed few problems in SI,TUF

--- Heaps/ReplaceElementsByRank.java
+++ Heaps/ReplaceElementsByRank.java
@@ -1,21 +1,21 @@
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class ReplaceElementsByRank {
+// public class ReplaceElementsByRank {
 
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int t = s.nextInt();
-        while (t-- > 0) {
-            System.err.println("Enter n : ");
-            int n = s.nextInt();
-            System.err.println("Enter ele's : ");
-            int a[] = new int[n];
-            for (int i = 0; i < n; i++) {
-                a[i] = s.nextInt();
-            }
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+//         int t = s.nextInt();
+//         while (t-- > 0) {
+//             System.err.println("Enter n : ");
+//             int n = s.nextInt();
+//             System.err.println("Enter ele's : ");
+//             int a[] = new int[n];
+//             for (int i = 0; i < n; i++) {
+//                 a[i] = s.nextInt();
+//             }
             
-        }
-        s.close();
-    }
-}
+//         }
+//         s.close();
+//     }
+// }
