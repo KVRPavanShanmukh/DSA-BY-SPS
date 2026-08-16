916d33b20db41ec9bf113f1515839d892c319d59
KVRPavanShanmukh <2300090201@kluniversity.in>
2026-08-16 22:39:04 +0530

    Greedy almost completed!

---
+++ Greedy/Merge_Intervals.java
@@ -0,0 +1,10 @@
+public class Merge_Intervals {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        //  [0,0]   --> st time of event 0
+        //  [0,1]   --> en time of event 0
+        
+    }
+    public static void main(String[] args) {
+
+    }
+}
